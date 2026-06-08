8076cb7f9616d5e888dc5b725bd92c5a7c383360
MD. MASUD RANA <102610611043@student.wub.edu.bd>
2026-06-09 00:44:27 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,96 @@
+// Smooth reveal animation
 
+const cards = document.querySelectorAll(".card");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+
+            entry.target.style.opacity = "1";
+            entry.target.style.transform = "translateY(0)";
+
+        }
+
+    });
+
+}, {
+    threshold: 0.2
+});
+
+cards.forEach((card) => {
+
+    card.style.opacity = "0";
+    card.style.transform = "translateY(50px)";
+    card.style.transition = "all 0.8s ease";
+
+    observer.observe(card);
+
+});
+
+// Navbar background on scroll
+
+window.addEventListener("scroll", () => {
+
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+
+        navbar.style.background =
+            "rgba(2,6,23,0.95)";
+
+    } else {
+
+        navbar.style.background =
+            "rgba(15,23,42,0.7)";
+    }
+
+});
+
+// Active menu link
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop =
+            section.offsetTop - 150;
+
+        if (scrollY >= sectionTop) {
+
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (
+            link.getAttribute("href") ===
+            "#" + current
+        ) {
+
+            link.classList.add("active");
+        }
+
+    });
+
+});
+
+// Welcome message
+
+window.addEventListener("load", () => {
+
+    console.log(
+        "Welcome to Masud Rana Portfolio"
+    );
+
+});
