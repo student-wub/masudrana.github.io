77926c563d010de6cfab4dcce5df427ad6e3dfc2
MD. MASUD RANA <102610611043@student.wub.edu.bd>
2026-06-09 14:05:13 +0600

    Update script.js

--- script.js
+++ script.js
@@ -191,3 +191,32 @@ counters.forEach(counter => {
 console.log(
 "Masud Rana Portfolio Loaded Successfully"
 );
+// Scroll To Top
+
+const scrollBtn =
+document.getElementById("scrollTop");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+
+        scrollBtn.style.display = "block";
+
+    }else{
+
+        scrollBtn.style.display = "none";
+
+    }
+
+});
+
+scrollBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top:0,
+        behavior:"smooth"
+
+    });
+
+});
